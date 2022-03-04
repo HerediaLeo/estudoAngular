017a89cf1d5de9e734c9a69f491470ed06c33527
HerediaLeo <lheredia1195@gmail.com>
2022-03-04 15:47:05 -0300

    Primeiro componente

---
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-nova-transferencia', //nome do componente.
+    templateUrl: './nova-transferencia.component.html', // aonde esta localicado o template deste componente.
+    styleUrls: ['./nova-transferencia.component.scss'] //css aplicado a este componente. Um componente pode ter mais de um arquivo css.
+  })
+
+
+export class NovaTransferenciaComponent{
+
+}
+
